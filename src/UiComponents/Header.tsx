db662e62d8387306be40438bc2871d25794bb340
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2024-12-23 20:22:52 +0530

    fixed header navigation

--- src/UiComponents/Header.tsx
+++ src/UiComponents/Header.tsx
@@ -1,19 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getMenuFromSupabase } from "../apis/apiRestaurant";
 import SearchOrder from "../features/order/components/SearchOrder";
-import Username from "../features/users/Username";
-import SearchBar from "./SearchBar";
 
 
 export default function Header() {
+  const location=useLocation();
+  console.log(location.pathname);
   return (
     <div className="bg-yellow-400 h-14 flex justify-between items-center px-6 py-3" onClick={()=>getMenuFromSupabase()}>
         <Link to="./">Pizza-On-Time Co.</Link>
         {/* <SearchBar ></SearchBar> */}
         <SearchOrder></SearchOrder>
         <div className="flex gap-4  px-3">
-          
+          {
+            (location.pathname==="/menu"||location.pathname!=="/me/orders")&&
+            
         <Link className="uppercase text-sm font-semibold " to={'/me/orders'}>my orders</Link>
+          }
+          {
+            location.pathname==="/me/orders"&&
+            
+        <Link className="uppercase text-sm font-semibold " to={'/menu'}>Menu</Link>
+          }
         <span className="uppercase text-sm font-semibold">|</span>
         <span className="uppercase text-sm font-semibold">satyen</span>
         </div>
